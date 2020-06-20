34adc2bd5e5e02b45d324f744cc7059bd3a4d777
PixsaOJ <sabasaba101010@gmail.com>
2020-06-21 00:56:46 +0400

    Communication Between Forms

--- src/components/FormAddress.vue
+++ src/components/FormAddress.vue
@@ -1,49 +1,61 @@
 <template>
-  <div>
+<div>
     <h1 class="title">Delivery details</h1>
 
     <h2 class="subtitle">
-      Where should we send your freshly roasted coffee beans?
+        Where should we send your freshly roasted coffee beans?
     </h2>
 
-    <form class="form">
-      <div class="form-group">
-        <label class="form-label" for="delivery_name">Name</label>
-        <input v-model="$v.form.recipient.$model" type="text" placeholder="Recipients Name" class="form-control" id="delivery_name">
-        <div v-if="$v.form.recipient.$error" class="error">field is required</div>
-      </div>
+    <form class="form" @input="submit">
+        <div class="form-group">
+            <label class="form-label" for="delivery_name">Name</label>
+            <input v-model="$v.form.recipient.$model" type="text" placeholder="Recipients Name" class="form-control" id="delivery_name">
+            <div v-if="$v.form.recipient.$error" class="error">field is required</div>
+        </div>
 
-      <div class="form-group">
-        <label class="form-label" for="address">Address</label>
-        <textarea v-model="$v.form.address.$model" placeholder="London Street 470978 New England" rows="3" class="form-control" id="address"></textarea>
-        <div v-if="$v.form.address.$error" class="error">field is required</div>
-      </div>
+        <div class="form-group">
+            <label class="form-label" for="address">Address</label>
+            <textarea v-model="$v.form.address.$model" placeholder="London Street 470978 New England" rows="3" class="form-control" id="address"></textarea>
+            <div v-if="$v.form.address.$error" class="error">field is required</div>
+        </div>
     </form>
-  </div>
+</div>
 </template>
 
 <script>
-  import {required} from 'vuelidate/lib/validators'
-  export default {
-    data () {
-      return {
-        form: {
-          address: null,
-          recipient: null
+import {
+    required
+} from 'vuelidate/lib/validators'
+export default {
+    data() {
+        return {
+            form: {
+                address: null,
+                recipient: null
+            }
         }
-      }
     },
     validations: {
-      form: {
-        address: {
-          required
-        },
-        recipient: {
-          required
+        form: {
+            address: {
+                required
+            },
+            recipient: {
+                required
+            }
+        }
+    },
+    methods: {
+      submit() {
+        if (!this.$v.$invalid) {
+          this.$emit('update', {
+            recipient: this.form.recipient,
+            address: this.form.address
+          })
         }
       }
     }
-  }
+}
 </script>
 
 <style scoped>
